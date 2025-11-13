6395dda85238fda3341416a613de88e424a573cd
Swapna-Karlawad <swapnakarlawad@gmail.com>
2025-11-13 16:58:24 +0530

    Add employee details input and display
    Fix variable names in print statements to match inputs.

---
+++ Emp.py
@@ -0,0 +1,8 @@
+print("Employee Details")
+Emp_Name = input("Enter your employee name: ")
+Emp_Salary = input("Enter your employee salary: ")
+Emp-ID = input("Enter your employee id: ")
+
+print(f"Emp_Name: {name}")
+print(f"Emp_Salary: {salary}")
+print(f"Emp_ID: {id}")
